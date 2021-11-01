82cc2934547fec46b659d6ce7a492bd0a7d6a5e3
Pranjal <pranjalgain052@gmail.com>
2021-11-01 18:40:40 +0530

    Libman added to directiory

---
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBooksById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title, string author)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(author)).ToList();
+        }
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel() { Id = 1, Title = "Book1", Author = "Author1" },
+                new BookModel() { Id = 2, Title = "Book2", Author = "Author2" },
+                new BookModel() { Id = 3, Title = "Book3", Author = "Author3" },
+                new BookModel() { Id = 4, Title = "Book4", Author = "Author4" },
+                new BookModel() { Id = 5, Title = "Book5", Author = "Author5" },
+            };
+        }
+    }
+}
--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,6 +21,8 @@ namespace BookStore
             services.AddControllersWithViews();
         }
 
+        
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -26,6 +30,8 @@ namespace BookStore
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
